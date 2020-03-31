23ae5761cda6cf3a52eaf34b256cc4a7a98228f4
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2020-03-31 13:39:49 +0430

    add func for choose my answer in questions.

--- src/Components/QuestionBox.js
+++ src/Components/QuestionBox.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./QuestionBox.scss";
 const QuestionBox = (props) => {
+    const [answer, setAnswer] = useState(props.options);
+    // const chooseAnswer = () => {
+    //     setAnswer([props.text]);
+    // }
     return (
         <div className="questionBox">
             <div className="questions">
                 {props.question}
+            </div>
+            <div className="answers">
+                {answer.map((text, index) => (
+                    <button key={index} className="answerBtn" onClick={() => {
+                        setAnswer([text]);
+                    }}>{text}</button>
+                ))}
             </div>
         </div>
     );
