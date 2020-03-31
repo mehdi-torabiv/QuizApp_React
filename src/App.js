0d89b590ee9bf41d26024271c38049257aa8d985
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2020-03-31 13:13:03 +0430

    map questions to page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ class App extends Component {
     questionBank: []
   };
   getQuestions = () => {
-    Querys().then(question => {
+    Querys().then(questions => {
       this.setState({
-        questionBank: question
+        questionBank: questions
       });
     });
+  }
+  componentDidMount() {
+    this.getQuestions();
+    console.log("[Questions] are Mount!", this.state.questionBank);
   }
   render() {
     return (
@@ -18,6 +22,11 @@ class App extends Component {
         <div className="QuizeSec">
           <div className="HeaderPage">
             <h4>Quiz App</h4>
+          </div>
+          <div className="questionsSec">
+            {this.state.questionBank.length > 0 &&
+              this.state.questionBank.map(({ questions, answers, correct, questionId }) =>
+                <h6>{questions}</h6>)}
           </div>
         </div>
       </div>
